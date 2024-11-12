f9ae16c01657cfd9a436ff2cf36c735206b899e0
gusneves <gustavonpl@protonmail.com>
2024-11-12 00:56:31 -0300

    Added client and server

--- src/client.rs
+++ src/client.rs
@@ -1,49 +1,45 @@
 use std::env;
-use futures::pin_mut;
-use futures_util::{future, StreamExt};
+
+use futures_util::{future, pin_mut, StreamExt};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio_tungstenite::tungstenite::Message;
-
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut stdin = tokio::io::stdin();
-
-    loop{
-        let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
-            Ok(n) => n
-        };
-
-        buf.truncate(n);
-        tx.unbounded_send(Message::binary(buf)).unwrap();
-    }
-}
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = env::args().nth(1).expect("Missing url argument");
+async fn main() {
+    let url =
+        env::args().nth(1).unwrap_or_else(|| panic!("this program requires at least one argument"));
 
     let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-
     tokio::spawn(read_stdin(stdin_tx));
 
-    let (web_socket_stream, _) = tokio_tungstenite::connect_async(url).await.expect("Failed to connect");
+    let (ws_stream, _) = connect_async(&url).await.expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
 
-    let(write, read) = web_socket_stream.split();
+    let (write, read) = ws_stream.split();
 
-    let stdin_to_web_socket = stdin_rx.map(Ok).forward(write);
-
-    let web_socket_to_stdout = {
+    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+    let ws_to_stdout = {
         read.for_each(|message| async {
             let data = message.unwrap().into_data();
             tokio::io::stdout().write_all(&data).await.unwrap();
         })
     };
 
-    pin_mut!(stdin_to_web_socket, web_socket_to_stdout);
-
-    future::select(stdin_to_web_socket, web_socket_to_stdout).await;
-
-    Ok(())
+    pin_mut!(stdin_to_ws, ws_to_stdout);
+    future::select(stdin_to_ws, ws_to_stdout).await;
 }
 
+// Our helper method which will read data from stdin and send it along the
+// sender provided.
+async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    let mut stdin = tokio::io::stdin();
+    loop {
+        let mut buf = vec![0; 1024];
+        let n = match stdin.read(&mut buf).await {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+        buf.truncate(n);
+        tx.unbounded_send(Message::binary(buf)).unwrap();
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -1,44 +1,93 @@
-mod client;
-
-use futures_util::{future, StreamExt, TryStreamExt};
-use log::*;
+use futures::{future, pin_mut, StreamExt, TryStreamExt};
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use std::{
+    collections::HashMap,
+    env, io,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{ AsyncWriteExt};
+use tokio_tungstenite::tungstenite::Message;
 
-async fn accept_connection(stream: TcpStream) {
-    let addr = stream.peer_addr().expect("Connected streams should have a peer address");
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
 
-    info!("Peer address: {}", addr);
-
-    let web_socket_stream = tokio_tungstenite::accept_async(stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
-
-    info!("New WebSocket connection: {}", addr);
-
-    let (write, read) = web_socket_stream.split();
-
-
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .forward(write)
-        .await
-        .expect("Failed to forward messages")
+/* struct ChannelManager {
+    channels: Arc<Mutex<std::collections::HashMap<String, Vec<mpsc::UnboundedSender<Message>>>>>,
 }
 
+impl ChannelManager {
+    fn new() -> Self {
+        ChannelManager {
+            channels: Arc::new(Mutex::new(std::collections::HashMap::new())),
+        }
+    }
+} */
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+async fn main() -> Result<(), io::Error> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
-    let addr = "127.0.0.1:9002";
+    let listener = TcpListener::bind(addr)
+        .await
+        .expect("Failed to start WebSocket server!");
 
-    let listener = TcpListener::bind(&addr).await.expect("Can't listen");
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
 
-    info!("Listening on: {}", addr);
+    println!("Websocket server started at ws://127.0.0.1:8080");
 
-    while let Ok((stream, _)) = listener.accept().await{
-        tokio::spawn(accept_connection(stream));
+    while let Ok((stream, addr)) = listener.accept().await {
+        // Handle each client connection asynchronously
+        tokio::spawn(handle_connection(state.clone(), stream, addr));
     }
 
     Ok(())
 }
 
+async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
+    println!("Incoming TCP connection from: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(raw_stream)
+        .await
+        .expect("Error during websocket handshake ocurred");
+
+    println!("WebSocket connection established with {}", addr);
+
+    let (unbounded_sender, unbounded_receiver) = unbounded::<Message>();
+
+    peer_map.lock().unwrap().insert(addr, unbounded_sender);
+
+    let (outgoing, incoming) = ws_stream.split();
+
+    let broadcast_incoming = incoming.try_for_each(|msg| {
+        println!(
+            "Received a message from {}: {}",
+            addr,
+            msg.to_text().unwrap()
+        );
+
+        let peers = peer_map.lock().unwrap();
+
+        let broadcast_recipients = peers
+            .iter()
+            .filter(|(peer_addr, _)| peer_addr != &&addr)
+            .map(|(_, ws_sink)| ws_sink);
+
+        for recp in broadcast_recipients {
+            recp.unbounded_send(msg.clone()).unwrap();
+        }
+
+        future::ok(())
+    });
+
+    let receive_from_others = unbounded_receiver.map(Ok).forward(outgoing);
+
+    pin_mut!(broadcast_incoming, receive_from_others);
+
+    future::select(broadcast_incoming, receive_from_others).await;
+
+    println!("{} disconnected", &addr);
+    peer_map.lock().unwrap().remove(&addr);
+}
